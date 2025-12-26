7aa5aea49b7286cbc760d1ad15524d4969a8d683
hyeon-aa <ahhyun1120@naver.com>
2025-12-27 07:25:04 +0900

    https

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  // 1. API 요청을 우회시키는 rewrite 설정 추가
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://43.201.18.138:8080/api/:path*',
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
