5488f0025a02049405e774abd1051ad97c1b88f0
hyeon-aa <ahhyun1120@naver.com>
2025-12-27 05:57:46 +0900

    vercel : img 수정

--- src/app/completepay/page.tsx
+++ src/app/completepay/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import GreenCheckIcon from '@/public/order/green-check.svg';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function CompletePayPage() {
@@ -9,7 +9,13 @@ export default function CompletePayPage() {
     <div className="flex flex-col items-center min-h-[100dvh] p-5">
       <div className="flex-1 flex flex-col items-center justify-center w-full">
         <div className="flex flex-col items-center">
-          <GreenCheckIcon className="mb-6" />
+          <Image
+            src="/order/green-check.svg"
+            alt="Success"
+            width={42}
+            height={42}
+            className="mb-6"
+          />
           <h1 className="text-heading-L-semibold mb-1">주문 완료!</h1>
           <p className="text-heading-S-regular text-[var(--color-gray-1000)] text-center">
             부스에서 주문 확인 중입니다.
